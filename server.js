8d3d5a6fffa43dce694e8167beea60c985f1c11d
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-15 23:17:16 -0400

    added inquirer prompts for menu

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ const db = mysql.createConnection(
 );
 
 function sqlCommandServer() {
-  connection.query("SELECT * from department", function(error, res) {
+  connection.query("SELECT * from department", function (error, res) {
     alldepartments = res.map(dept =>
-       ({ name: dept.name, value: dept.id }));
+      ({ name: dept.name, value: dept.id }));
   });
 
-  connection.query("SELECT * from role", function(error, res) {
+  connection.query("SELECT * from role", function (error, res) {
     allroles = res.map(role =>
-       ({ name: role.title, value: role.id }));
+      ({ name: role.title, value: role.id }));
   });
 
-  connection.query("SELECT * from employee", function(error, res) {
+  connection.query("SELECT * from employee", function (error, res) {
     allemployees = res.map(employee => ({
       name: `${employee.first_name} ${employee.last_name}`,
       value: employee.id
@@ -36,8 +36,64 @@ function sqlCommandServer() {
   });
 }
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("\nPlease Use This Program to Track your Employees\n");
+  startTracker();
   sqlCommandServer();
 });
+
+function startTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
